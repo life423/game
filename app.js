d58cb39fc9a078745d4babd0e856ff41c1c766d2
Andrew Clark <drew@drewclark.io>
2023-10-25 21:55:49 -0500

    feat: Refactor code for improved readability and maintainability
    - Changed variable declarations from `var` to `let` and `const`
    - Removed duplicate event listeners for keydown and keyup events
    - Created separate functions for keyDownHandler and keyUpHandler
    - Added comments to clarify the purpose of each function
    - Renamed drawSqaure() function to drawSquare()
    - Added JSDoc comments to Car class methods
    - Removed unnecessary comments and unused code

--- app.js
+++ app.js
@@ -1,65 +1,67 @@
-var canvas = document.getElementById("canvas"),
-  ctx = canvas.getContext("2d");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-var score = 0,
-  highScore = 0;
+let score = 0;
+let highScore = 0;
 
-var x = canvas.width / 2,
-  y = 488,
-  width = 30,
-  height = 30;
+let x = canvas.width / 2;
+let y = 488;
+const width = 30;
+const height = 30;
 
-var rightPressed = false,
-  leftPressed = false,
-  upPressed = false,
-  downPressed = false;
+let rightPressed = false;
+let leftPressed = false;
+let upPressed = false;
+let downPressed = false;
 
-var up = true,
-  down = true,
-  right = true,
-  left = true;
+let up = true;
+let down = true;
+let right = true;
+let left = true;
 
 const NUM_LANES = 5;
 const LANE_HEIGHT = canvas.height / NUM_LANES;
 
-var squareWidth = 20;
-var squareHeight = 20;
+const squareWidth = 20;
+const squareHeight = 20;
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
 
-//controls which key is pussed down and sets their values of up and down on and off
-function keyDownHandler(e) {
-  if (e.keyCode == 39) {
-    rightPressed = true;
+const keyDownHandler = (e) => {
+  const { keyCode } = e;
+  switch (keyCode) {
+    case 39:
+      rightPressed = true;
+      break;
+    case 37:
+      leftPressed = true;
+      break;
+    case 38:
+      upPressed = true;
+      break;
+    case 40:
+      downPressed = true;
+      break;
   }
-  if (e.keyCode == 37) {
-    leftPressed = true;
-  }
-  if (e.keyCode == 38) {
-    upPressed = true;
-  }
-  if (e.keyCode == 40) {
-    downPressed = true;
-  }
-}
+};
 
-function keyUpHandler(e) {
-  if (e.keyCode == 39) {
-    rightPressed = false;
+const keyUpHandler = (e) => {
+  const { keyCode } = e;
+  switch (keyCode) {
+    case 39:
+      rightPressed = false;
+      break;
+    case 37:
+      leftPressed = false;
+      break;
+    case 38:
+      upPressed = false;
+      break;
+    case 40:
+      downPressed = false;
+      break;
   }
-  if (e.keyCode == 37) {
-    leftPressed = false;
-  }
-  if (e.keyCode == 38) {
-    upPressed = false;
-  }
-  if (e.keyCode == 40) {
-    downPressed = false;
-  }
-}
+};
 
-//makes the frog move with each tap of an arrow key instead of holding down an arrow key
 function moveFrong() {
   if (upPressed === true && up === true && y > 20) {
     y = y - 40;
@@ -95,7 +97,6 @@ function moveFrong() {
   }
 }
 
-//draws the initial piece
 function drawSqaure() {
   ctx.beginPath();
   ctx.rect(x, y, 20, 20);
@@ -103,15 +104,23 @@ function drawSqaure() {
   ctx.fill();
 }
 
-//class to produce each square
+/**
+ * Represents a car in the game.
+ * @constructor
+ * @param {number} width - The width of the car.
+ * @param {number} lane - The lane in which the car is located.
+ */
 class Car {
   constructor(width, lane) {
     this.x = x;
     this.y = lane * LANE_HEIGHT;
     this.width = width;
-    this.speed = Math.random() * 2 + 1; // Assign a constant speed between 1 and 3
+    this.speed = Math.random() * 2 + 2;
   }
 
+  /**
+   * Draws the car on the canvas.
+   */
   draw() {
     ctx.beginPath();
     ctx.rect(this.x, this.y, this.width, 25);
@@ -119,47 +128,41 @@ class Car {
     ctx.fill();
   }
 
+  /**
+   * Updates the position of the car on the canvas.
+   */
   update() {
-    this.x += this.speed; // Use the constant speed to update the position
+    this.x += this.speed;
     if (this.x > canvas.width) {
-      this.x = -this.width; // Reset the position to the left edge when the car goes off the right edge
+      this.x = -this.width;
     }
     this.draw();
   }
 }
 
-//an array all cars get pushed into from the addCar() function
-var cars = [
+const cars = [
   new Car(
     randomIntFromInterval(40, 100),
     Math.floor(Math.random() * NUM_LANES)
   ),
 ];
 
-//checks if a rectange and a square are overlapping on the canvas
-// Detect collisions
 function detectCollision() {
-  // Iterate over each car
   cars.forEach(function (car) {
-    // Define the player's edges
     const playerLeft = x;
     const playerRight = x + width;
     const playerTop = y;
     const playerBottom = y + height;
 
-    // Define the car's edges
     const carLeft = car.x;
-    const carRight = car.x + car.z; // Assuming car.z is the car's width
+    const carRight = car.x + car.z;
     const carTop = car.y;
-    const carBottom = car.y + 25; // Assuming 25 is the car's height
+    const carBottom = car.y + 25;
 
-    // Check if the car and the player overlap
     const collisionX = carLeft < playerRight && carRight > playerLeft;
     const collisionY = carTop < playerBottom && carBottom > playerTop;
 
-    // If the car and the player overlap, the player has collided with the car
     if (collisionX && collisionY) {
-      // Reset the score
       score = 0;
       cars.length = 1;
       document.getElementById("score").innerHTML = score;
@@ -167,7 +170,6 @@ function detectCollision() {
     }
   });
 }
-
 
 function checkForWinner() {
   if (y < 10) {
@@ -180,33 +182,28 @@ function checkForWinner() {
   }
 }
 
-//generates a random number between a range
 function randomIntFromInterval(min, max) {
-  // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-//displays the squares that it is told to create from the addCar() function
 function displaySquares() {
   cars.forEach(function (car) {
     car.update();
   });
 }
 
-//adds cars based on the players score
 function addCar() {
   if (score <= 2) {
-    const lane = Math.floor(Math.random() * NUM_LANES); // Choose a random lane
+    const lane = Math.floor(Math.random() * NUM_LANES);
     cars.push(new Car(randomIntFromInterval(40, 100), lane));
   }
 
   if (score % 4 === 0) {
-    const lane = Math.floor(Math.random() * NUM_LANES); // Choose a random lane
+    const lane = Math.floor(Math.random() * NUM_LANES);
     cars.push(new Car(randomIntFromInterval(40, 100), lane));
   }
 }
 
-//updates the high score on page
 function getHighScore() {
   if (score > highScore) {
     highScore = score;
@@ -214,7 +211,6 @@ function getHighScore() {
   }
 }
 
-//calls essential functions in an infinite animation loop
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -225,4 +221,9 @@ function animate() {
   getHighScore();
   checkForWinner();
 }
+
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
 animate();
